9fd09834a497d36a3c720abb5305b9362c89fe5b
franvergara10 <vergarafran12345@gmail.com>
2025-03-04 12:29:52 +0100

    .

--- src/equipo/jugador/centrocampista/Centrocampista.java
+++ src/equipo/jugador/centrocampista/Centrocampista.java
@@ -3,33 +3,21 @@ package equipo.jugador.centrocampista;
 import equipo.Equipo;
 import equipo.jugador.Jugador;
 
-public class Centrocampista extends Jugador{
-	
+public abstract class Centrocampista extends Jugador {
+
 	private int pasesCompletados;
-	
+
 	public Centrocampista(String nombre, int dorsal, Equipo equipo, int pasesCompletados) {
 		super(nombre, dorsal, equipo);
 		setPasesCompletados(pasesCompletados);
 	}
-	
-	
 
 	public int getPasesCompletados() {
 		return pasesCompletados;
 	}
 
-
-
 	public void setPasesCompletados(int pasesCompletados) {
 		this.pasesCompletados = pasesCompletados;
-	}
-
-
-
-	@Override
-	public void mostrarDatos() {
-		System.out.println("Nombre:");
-		
 	}
 
 }
---
+++ src/equipo/jugador/centrocampista/Pivote.java
@@ -0,0 +1,33 @@
+package equipo.jugador.centrocampista;
+
+import equipo.Equipo;
+
+public class Pivote extends Centrocampista {
+	
+	private int intercepciones;
+
+	public Pivote(String nombre, int dorsal, Equipo equipo, int pasesCompletados, int intercepciones) {
+		super(nombre, dorsal, equipo, pasesCompletados);
+		setIntercepciones(intercepciones);
+	}
+
+	public int getIntercepciones() {
+		return intercepciones;
+	}
+
+	public void setIntercepciones(int intercepciones) {
+		this.intercepciones = intercepciones;
+	}
+
+	@Override
+	public void mostrarDatos() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
+
+}
