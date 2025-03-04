0db3c195153fcb8856f649cd870157126cbdce70
franvergara10 <vergarafran12345@gmail.com>
2025-03-04 11:04:39 +0100

    .

--- src/equipo/jugador/Jugador.java
+++ src/equipo/jugador/Jugador.java
@@ -2,12 +2,42 @@ package equipo.jugador;
 
 import equipo.Equipo;
 
-public class Jugador {
+public abstract class Jugador {
 	
 	private String nombre;
 	private int dorsal;
 	private Equipo equipo;
 	
+	public Jugador(String nombre, int dorsal, Equipo equipo) {
+		setNombre(nombre);
+		setDorsal(dorsal);
+		setEquipo(equipo);
+	}
 	
+	public String getNombre() {
+		return nombre;
+	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+
+	public Equipo getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(Equipo equipo) {
+		this.equipo = equipo;
+	}
+
+	public abstract void mostrarDatos();
+	
 }
